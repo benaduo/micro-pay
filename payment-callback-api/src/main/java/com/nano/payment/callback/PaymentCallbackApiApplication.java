ef20ba49890b66c1f5f295c4d5af82a373284185
benaduo <manizza14@gmail.com>
2025-12-27 22:33:40 +0000

    Add Payment API applications and update pom.xml for shared dependency

---
+++ payment-callback-api/src/main/java/com/nano/payment/callback/PaymentCallbackApiApplication.java
@@ -0,0 +1,11 @@
+package com.nano.payment.callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCallbackApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCallbackApiApplication.class, args);
+    }
+}
---
+++ payment-consumer/src/main/java/com/nano/payment/consumer/PaymentConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.nano.payment.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentConsumerApplication.class, args);
+    }
+}
