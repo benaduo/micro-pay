f5a71c0748d5ff6e0411cb0c37777ec2274f8972
benaduo <manizza14@gmail.com>
2025-12-28 09:28:40 +0000

    Add ApiResponse and BaseEntity classes, and include Spring Data JPA dependency in pom.xml

---
+++ shared/src/main/java/com/nano/shared/dtos/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.nano.shared.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T>{
+    private boolean isSuccess;
+    private String message;
+    private T data;
+    private String errorCode;
+}
---
+++ shared/src/main/java/com/nano/shared/entities/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.nano.shared.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
